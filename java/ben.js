0c33657e2d048a48e8851ad412d638d0ac69aa10
Kiebertje <90263552+Kiebertje@users.noreply.github.com>
2021-09-26 11:42:41 +0200

    Add files via upload

---
+++ java/ben.js
@@ -0,0 +1,24 @@
+/*button*/
+/*
+button = document.querySelector ("button"); 
+
+button.addEventListener ("click", verander);
+
+function verander() {
+button.classList.add("mooieding")
+} */
+
+
+
+/*hamburgermenu */
+/*hulp van Alex Lanting klasgenoot */
+
+menu = document.querySelector ("nav ul li:nth-of-type(1)"); 
+headerul = document.querySelector("header > ul:nth-of-type(1)")
+
+
+menu.addEventListener("click", hamburgermenu);
+
+function hamburgermenu() {
+headerul.classList.toggle("hamburgermenu")
+}
