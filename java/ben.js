77df0be6f425a3fd23201b978fed0f3caf5a7621
Kiebertje <90263552+Kiebertje@users.noreply.github.com>
2021-09-29 22:36:34 +0200

    Add files via upload

--- java/ben.js
+++ java/ben.js
@@ -12,7 +12,6 @@ button.classList.add("mooieding")
 
 /*hamburgermenu */
 /*hulp van Alex Lanting klasgenoot */
-
 menu = document.querySelector ("nav ul li:nth-of-type(1)"); 
 headerul = document.querySelector("header > ul:nth-of-type(1)")
 
@@ -22,3 +21,4 @@ menu.addEventListener("click", hamburgermenu);
 function hamburgermenu() {
 headerul.classList.toggle("hamburgermenu")
 }
+
